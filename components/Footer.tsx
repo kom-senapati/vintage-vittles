e510a7e98065b9ee3996802ef5dd9ecd1e062a42
K Om Senapati <komnoob123@gmail.com>
2024-12-14 16:31:51 +0530

    chore: Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="nes-container" style={{ backgroundColor: 'var(--light-pink)' }}>
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <p>&copy; 2024 Recipe Genie. All rights reserved.</p>
+        <p>&copy; 2024 Vintage Vittles. All rights reserved.</p>
         <div className="flex space-x-4 mt-4 md:mt-0">
           <Link href="/about" className="nes-btn">About</Link>
           <Link href="/contact" className="nes-btn">Contact</Link>
